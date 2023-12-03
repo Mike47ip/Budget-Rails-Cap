b7d0c702d76560fad24e9dcd99d2d26b528bd6fc
Mike Dark <agyemankojo47@gmail.com>
2023-12-03 11:34:03 +0000

    setup render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  
+# Disable serving static files from the `/public` folder by default since
+# Apache or NGINX already handles this.
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 end
